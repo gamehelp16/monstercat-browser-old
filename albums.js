ccc7e6ca780b47b0cb13350e9d081fcb8418eb96
gamehelp16 <richardsudaryono@gmail.com>
2016-07-09 23:20:16 +0800

    Added LP support and a lot more!

--- albums.js
+++ albums.js
@@ -1,1 +1,34 @@
-availableAlbums = ["001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012", "013", "014", "015", "016", "017", "018", "019", "020", "021", "022", "023", "024", "025", "026", "027"];
+var compilations = 27;
+var lps = 8;
+
+var albumData = {};
+var request = new XMLHttpRequest();
+request.open('GET', 'albums/overview.json', true);
+
+request.onload = function() {
+	albumData = JSON.parse(request.responseText);
+	showGrids();
+};
+
+request.onerror = function() {
+	alert('Error: cannot retrieve album data.');
+};
+
+request.send();
+
+availableCompilations = [];
+for(var i=1;i<=compilations;i++) {
+	var pad = "0";
+	if(i > 99) pad = "";
+	else if(i < 10) pad = "00";
+	var albumnumber = pad + i;
+	availableCompilations.push(albumnumber);
+}
+
+availableLPs = [];
+for(var i=1;i<=lps;i++) {
+	var pad = "0";
+	if(i > 99) pad = "";
+	else if(i < 10) pad = "00";
+	availableLPs.push("MCLP" + pad + i);
+}
